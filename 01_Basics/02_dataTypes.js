c332b3df244de44fd2befa7162e3d640cb8e0770
Tushar Parashar <105961195+tusharparashar02@users.noreply.github.com>
2024-01-18 12:31:24 +0000

    container config done

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,12 @@
+"use strict"// treat all js code as newer version
+// alert(3+3) // we are using nodejs not browser
+
+//--> PREMITIVE DATATYPE <--
+// number , bigint, string=>" " , boolean=>true/false
+//null=> standalone value, undefined, symbol=> unique
+
+
+//--> OBJECT<--
+console.log(typeof "age"); // string
+console.log(typeof undefined) // undefined
+console.log(typeof null); // object
